c79872a5e648f81ef8da9d2983a0fc56eabfda58
Brandon <bwinkler4@gmail.com>
2023-06-16 10:20:14 -0700

    touched up navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
     return (
         <nav className='bg-white h-24'>
-        <div className='flex justify-center items-center h-full'>
-        <ul className='flex items-center gap-[3vw]'>
-            <li><button type='button' className='bg-white font-bebas hover:underline text-3xl'>Home</button></li>
-            <li><Link to='/reviews' type='button' className='bg-white font-bebas hover:underline text-3xl'>Reviews</Link></li>
-            <li><Link to='/used-gear' type='button' className='bg-white font-bebas hover:underline text-3xl'>Used Gear</Link></li>
-            <li><Link to='/login-signup' type='button' className='bg-white font-bebas hover:underline text-3xl'>Login/Sign-Up</Link></li>
-        </ul>
-        </div>
-      </nav>
+            <div className='flex justify-center items-center h-full'>
+            <ul className='flex items-center gap-[3vw]'>
+                <li><Link to='/' type='button' className='bg-white font-bebas hover:underline text-3xl'>Home</Link></li>
+                <li><Link to='/reviews' type='button' className='bg-white font-bebas hover:underline text-3xl'>Reviews</Link></li>
+                <li><Link to='/used-gear' type='button' className='bg-white font-bebas hover:underline text-3xl'>Used Gear</Link></li>
+                <li><Link to='/login-signup' type='button' className='bg-white font-bebas hover:underline text-3xl'>Login/Sign-Up</Link></li>
+            </ul>
+            </div>
+        </nav>
     );
 }
 
