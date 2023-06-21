e42c6d8da969b8d71759c4e0b81e19699ac6ce35
Brandon <bwinkler4@gmail.com>
2023-06-21 16:12:20 -0700

    more side bar work, set up the categories and brands just need to do ratings and date filters

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -7,8 +7,6 @@ const SideBar = () => {
 
     return (
         <>
-            <h1 className="text-xl opacity-50 pl-10 py-5">Home /</h1>
-            <h1 className="pl-10 text-6xl font-bebas">Reviews</h1>
             {!isOpen ?
                 (
                     <div className="pl-10">
@@ -24,17 +22,63 @@ const SideBar = () => {
                     <button className="text-white z-50 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>x</button>
                 )
             }
-
             <div
                 className={`top-0 left-0 w-[20vw] bg-white fixed h-full z-40  ease-in-out duration-300 
                 ${isOpen ? "translate-x-0" : "-translate-x-full"}`} >
-                    <h1 className="text-4xl font-semibold text-black pl-5 pt-5">Filters</h1>
-                <button
-                    className="text-white z-50 cursor-pointer pl-5 bg-black justify-end"
-                    onClick={() => setIsOpen(false)}
-                >
-                    x
-                </button>
+                    <div className="border-b-2 border-black">
+                        <h1 className="text-4xl font-semibold text-black pl-5 py-5">Filters</h1>
+                    </div>
+                    <div className="border-b-2 pb-5">
+                        <h1 className="text-lg pl-5 py-4 font-medium">Category</h1>
+                        <ul className="pl-5">
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Barbells </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Weights </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Benches </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Dumbbells </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Racks </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Machines </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Conditioning </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Strongman </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Crossfit</button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Gymnastics </button>
+                        </ul>
+                    </div>
+                    <div className="border-b-2 pb-5">
+                        <h1 className="text-lg pl-5 py-4 font-medium">Brands</h1>
+                        <ul className="pl-5">
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Rogue </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Rep </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Sorinex </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Titan </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Peloton </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">EliteFTS </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">NordicTrack </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">LifeFitness </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">Eleiko </button>
+                            <button className="transition-all ease-in-out duration-150 rounded border-2 px-4 border-black bg-white hover:invert">TRX </button>
+                        </ul>
+                    </div>
+                    <div className="border-b-2 pb-5">
+                        <h1 className="text-lg pl-5 py-4 font-medium">Ratings</h1>
+                        <ul className="pl-5 flex flex-col items-start">
+                            <div>
+                                <input type="checkbox" />
+                            </div>
+                            <div>
+                                <input type="checkbox" />
+                            </div>
+                            <div>
+                                <input type="checkbox" />
+                            </div>
+                            <div>
+                                <input type="checkbox" />
+                            </div>
+                            <div>
+                                <input type="checkbox" />
+                            </div>
+                            
+                        </ul>
+                    </div>
             </div>
             <div
                 className={`fixed top-0 left-0 w-full h-full bg-black opacity-0 transition-opacity duration-300 ${
--- src/pages/ReviewsPage.tsx
+++ src/pages/ReviewsPage.tsx
@@ -6,6 +6,10 @@ const ReviewsPage = () => {
     return(
         <div>
             <NavBar></NavBar>
+            <div className='pl-10 pt-10 flex flex-row'>
+                <h1 className="pt-3 text-xl opacity-50">Home /</h1>
+                <h1 className=" pl-6 text-6xl font-bebas">Reviews</h1>
+            </div>
             <SideBar></SideBar>
         </div>
     );
