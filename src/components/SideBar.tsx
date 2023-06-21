b76562b4fd8e9b2ab81172e94e8a93aea6638967
Brandon <bwinkler4@gmail.com>
2023-06-20 18:07:33 -0700

    more work on side bar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,30 +1,47 @@
 import { useState } from "react";
-
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 const SideBar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <>
+            <h1 className="text-xl opacity-50 pl-10 py-5">Home /</h1>
+            <h1 className="pl-10 text-6xl font-bebas">Reviews</h1>
             {!isOpen ?
                 (
-                    <button onClick={() => setIsOpen(!isOpen)}>not open</button>
+                    <div className="pl-10">
+                        <button 
+                            onClick={() => setIsOpen(!isOpen)} 
+                            className="border-2 border-black rounded-none w-12 h-12"
+                        >
+                            <FontAwesomeIcon icon={faFilter} className="text-black text-2xl" />
+                        </button>
+                    </div>
                 ) :
                 (
                     <button className="text-white z-50 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>x</button>
                 )
             }
+
             <div
-                className={`top-0 left-0 w-[15vw] bg-blue-600 text-white fixed h-full z-40  ease-in-out duration-300 
+                className={`top-0 left-0 w-[20vw] bg-white fixed h-full z-40  ease-in-out duration-300 
                 ${isOpen ? "translate-x-0" : "-translate-x-full"}`} >
+                    <h1 className="text-4xl font-semibold text-black pl-5 pt-5">Filters</h1>
                 <button
-                    className="text-white z-50 cursor-pointer"
+                    className="text-white z-50 cursor-pointer pl-5 bg-black justify-end"
                     onClick={() => setIsOpen(false)}
                 >
                     x
                 </button>
-                <h1 className="text-4xl font-semibold text-white">I am a sidebar</h1>
             </div>
-
+            <div
+                className={`fixed top-0 left-0 w-full h-full bg-black opacity-50 transition-opacity duration-300 ${
+                    isOpen ? "opacity-50 pointer-events-auto" : "opacity-0 pointer-events-none"
+                }`}
+                onClick={() => setIsOpen(false)}
+            />
         </>
 
 
