d99b05ec31de36bd1bc99e4dbf70f701d56e6746
Brandon <bwinkler4@gmail.com>
2023-07-03 16:18:23 -0700

    created event listeners that use custom css so that the form is reponsive on login and signup button clicks. Also switched the links in the navbar to a tags so that the css events load every time you navigate between pages.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,11 @@
-import { Link } from "react-router-dom";
-
 const NavBar = () => {
     return (
         <nav className='h-24 z-10 sticky top-0'>
             <div className='flex justify-center items-center h-full'>
             <ul className='flex items-center gap-[3vw]'>
-                <li><Link to='/' type='button' className='bg-transparent font-bebas hover:underline text-3xl text-white hover:text-FitYellow'>Home</Link></li>
-                <li><Link to='/used-gear' type='button' className='bg-transparent font-bebas hover:underline text-3xl text-white hover:text-FitYellow'>Post Used Gear</Link></li>
-                <li><Link to='/login-signup' type='button' className='bg-transparent font-bebas hover:underline text-3xl text-white hover:text-FitYellow'>Login/Sign-Up</Link></li>
+                <li><a href='/' type='button' className='bg-transparent font-bebas hover:underline text-3xl text-white hover:text-FitYellow'>Home</a></li>
+                <li><a href='/used-gear' type='button' className='bg-transparent font-bebas hover:underline text-3xl text-white hover:text-FitYellow'>Post Used Gear</a></li>
+                <li><a href='/login-signup' type='button' className='bg-transparent font-bebas hover:underline text-3xl text-white hover:text-FitYellow'>Login/Sign-Up</a></li>
             </ul>
             </div>
         </nav>
--- src/components/UserAuth.tsx
+++ src/components/UserAuth.tsx
@@ -1,15 +1,31 @@
 const UserAuth = () => {
+    document.addEventListener('DOMContentLoaded', () => {
+        const loginBtn = document.getElementById('loginBtn') as HTMLButtonElement;
+        const signupBtn = document.getElementById('signupBtn') as HTMLButtonElement;
+        const loginForm = document.getElementById('login') as HTMLFormElement;
+        const signupForm = document.getElementById('sign_up') as HTMLFormElement;
+
+        loginBtn.addEventListener('click', () => {
+            loginForm.classList.remove('moved');
+            signupForm.classList.add('moved');
+        });
+
+        signupBtn.addEventListener('click', () => {
+            loginForm.classList.add('moved');
+            signupForm.classList.remove('moved');
+        });
+    });
 
     return (
-        <div className="w-96 h-[32rem] z-20 bg-white border-black border-8 m-auto">
+        <div id="formContainer" className="w-96 h-[32rem] z-20 bg-white border-black border-8 m-auto">
             <div className="w-full h-[80px] bg-black flex items-center justify-center relative">
                 <div className="flex justify-center w-1/2 h-full">
-                    <button type="button" className="w-full cursor-pointer font-bebas px-8 py-4 text-2xl z-10 bg-black text-white hover:underline hover:invert focus:invert">
+                    <button type="button" id="loginBtn" className="w-full cursor-pointer font-bebas px-8 py-4 text-2xl z-10 bg-black text-white hover:underline hover:invert focus:invert">
                         LogIn
                     </button>
                 </div>
                 <div className="flex justify-center w-1/2 h-full">
-                    <button type="button" className="w-full cursor-pointer font-bebas px-8 py-4 text-2xl z-10 bg-black text-white hover:underline hover:invert focus:invert">
+                    <button type="button" id="signupBtn" className="w-full cursor-pointer font-bebas px-8 py-4 text-2xl z-10 bg-black text-white hover:underline hover:invert focus:invert">
                         Sign-up
                     </button>
                 </div>
@@ -26,7 +42,7 @@ const UserAuth = () => {
                         <button type="submit" className="bg-black text-white px-8 py-2 rounded font-roboto">Log In</button>
                     </div>
                 </form>
-                <form id="sign_up" className="flex flex-col pt-8 ml-56 overflow-hidden">
+                <form id="sign_up" className="flex flex-col pt-8 ml-56 ">
                     <div className="flex justify-center py-4 pt-4 h-16">
                         <input type="text" placeholder='First Name' className="border-b-2 border-black" />
                     </div>
