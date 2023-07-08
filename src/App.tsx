4bf076761de560c7106f07dca5a6fb07b569b451
Brandon <bwinkler4@gmail.com>
2023-07-07 21:40:46 -0700

    fixed the background image on the login/signup page so that the background image doesn't flex when window shrinks

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,17 @@ function App() {
 
   return (
     <>
-      {/* <div className='max-w h-[200px] relative group'> */}
+      <div className='max-w relative'>
         <video className='max-w' autoPlay loop>
           <source src={WelcomeVid} type="video/mp4" />
         </video>
-      {/* </div> */}
+      </div>
       <div className='absolute top-0 left-0 w-full z-10 hover:bg-black transition-all duration-300'>
         <NavBar></NavBar>
       </div>
-      <h1 className="text-center font-roboto py-10 text-2xl">Recent Activity</h1>
+      <h1 className="text-center font-roboto py-10 text-2xl">Recent Uploads</h1>
       <SideBar></SideBar>
+
     </>
   )
 }
--- src/pages/LoginSignupPage.tsx
+++ src/pages/LoginSignupPage.tsx
@@ -5,13 +5,15 @@ import authImage from "../assets/authImage.jpg"
 const LoginSignupPage = () => {
     return (
         <>
-            <div className='top-0 left-0 w-full z-10 bg-black'>
-                <NavBar></NavBar>
+            <div className='relative z-30'>
+                <div className='top-0 left-0 w-full bg-black'>
+                    <NavBar></NavBar>
+                </div>
             </div>
             <div className='relative'>
-                <img src={authImage} className='w-full h-full' alt='Authentication' />
+                <img src={authImage} className='w-screen h-screen object-cover' alt='Authentication' />
                 <div className='absolute top-0 left-0 w-full h-full z-20'>
-                    <div className="pt-40">
+                    <div className="pt-32">
                         <UserAuth></UserAuth>
                     </div>
                 </div>
